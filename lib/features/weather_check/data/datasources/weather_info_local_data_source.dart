df3d4b576ff47178edd5b9d218501543dbd028ec
Ildar Biglov <nj0rd3841@gmail.com>
2022-06-07 18:57:12 +0500

    end of repository impl

--- lib/features/weather_check/data/datasources/weather_info_local_data_source.dart
+++ lib/features/weather_check/data/datasources/weather_info_local_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:weather_info/core/error/exceptions.dart';
 import 'package:weather_info/features/weather_check/data/models/weather_info_model.dart';
 
+import '../../domain/entities/weather_info.dart';
+
 abstract class WeatherInfoLocalDataSource {
   ///Gets the cached [WeatherInfoModel] which was gotten the last time
   ///the user had an internet connection.
@@ -8,5 +10,5 @@ abstract class WeatherInfoLocalDataSource {
   /// Throws [CacheException] if no cached data is present.
   Future<WeatherInfoModel> getLastWeatherInfo();
 
-  Future<void> cachedWeatherInfo(WeatherInfoModel weatherToCache);
+  Future<void> cacheWeatherInfo(WeatherInfo weatherToCache);
 }
--- lib/features/weather_check/data/repositories/weather_info_repository_impl.dart
+++ lib/features/weather_check/data/repositories/weather_info_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:weather_info/core/error/exceptions.dart';
 import 'package:weather_info/core/error/failures.dart';
 import 'package:weather_info/features/weather_check/domain/entities/weather_info.dart';
 import 'package:weather_info/features/weather_check/domain/repositories/weather_info_repository.dart';
@@ -7,8 +8,12 @@ import '../../../../core/platform/network_info.dart';
 import '../datasources/weather_info_local_data_source.dart';
 import '../datasources/weather_info_remote_data_source.dart';
 
+typedef _ConcreteOrRandomChooser = Future<WeatherInfo> Function();
+
 class WeatherInfoRepositoryImpl implements WeatherInfoRepository{
   final WeatherInfoRemoteDataSource remoteDataSource;
+  final WeatherInfoLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
 
   WeatherInfoRepositoryImpl({
     required this.remoteDataSource,
@@ -16,19 +21,42 @@ class WeatherInfoRepositoryImpl implements WeatherInfoRepository{
     required this.networkInfo
   });
 
-  final WeatherInfoLocalDataSource localDataSource;
-  final NetworkInfo networkInfo;
-
   @override
-  Future<Either<Failure, WeatherInfo>> getWeatherByCityName(String city) {
+  Future<Either<Failure, WeatherInfo>> getWeatherByCityName(String city) async {
     // TODO: implement getWeatherByCityName
-    throw UnimplementedError();
+    return await _getWeather(() {
+      return remoteDataSource.getWeatherByCityName(city);
+    });
   }
 
   @override
-  Future<Either<Failure, WeatherInfo>> getWeatherByRandomCity() {
+  Future<Either<Failure, WeatherInfo>> getWeatherByRandomCity() async {
     // TODO: implement getWeatherByRandomCity
-    throw UnimplementedError();
+    return await _getWeather(() {
+      return remoteDataSource.getWeatherByRandomCity();
+    });
+  }
+
+  Future<Either<Failure, WeatherInfo>> _getWeather(
+      _ConcreteOrRandomChooser getConcreteOrRandom,
+      ) async {
+    if (await networkInfo.isConnected){
+      try{
+        final remoteWeatherInfo = await getConcreteOrRandom();
+        //TODO: check parameter should be model or entity
+        localDataSource.cacheWeatherInfo(remoteWeatherInfo);
+        return Right(remoteWeatherInfo);
+      }on ServerException {
+        return Left(ServerFailure());
+      }
+    }else{
+      try{
+        final localWeatherInfo = await localDataSource.getLastWeatherInfo();
+        return Right(localWeatherInfo);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 
 }
