412674194f92b2fc304ffb8903964c64ebd9001e
Ildar Biglov <nj0rd3841@gmail.com>
2022-06-13 14:19:48 +0500

    remote data sourse

--- lib/features/weather_check/data/datasources/weather_info_remote_data_source.dart
+++ lib/features/weather_check/data/datasources/weather_info_remote_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_info/core/error/exceptions.dart';
+
 import '../models/weather_info_model.dart';
 
 abstract class WeatherInfoRemoteDataSource {
@@ -12,4 +17,41 @@ abstract class WeatherInfoRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<WeatherInfoModel> getWeatherByRandomCity();
+}
+
+class WeatherInfoRemoteDataSourceImpl implements WeatherInfoRemoteDataSource {
+  final http.Client httpClient;
+
+  WeatherInfoRemoteDataSourceImpl({required this.httpClient});
+
+  @override
+  Future<WeatherInfoModel> getWeatherByCityName(String city) => _getWeatherInfo(city);
+
+  @override
+  Future<WeatherInfoModel> getWeatherByRandomCity() {
+    //TODO: implement choosing random city from cities.json
+    const _city = 'Moscow';
+
+    return _getWeatherInfo(_city);
+  }
+
+  Future<WeatherInfoModel> _getWeatherInfo(String city) async {
+    final result = await httpClient.get(
+        Uri.parse(
+            'http://api.weatherapi.com/v1/current.json?key=9c74fd81e58f44989c192430220406&q=$city&aqi=no'
+        ),
+        headers: {
+          'Content-type': 'application/json'
+        }
+    );
+
+    if(result.statusCode == 200){
+      return WeatherInfoModel.fromJson(jsonDecode(result.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  void _getCities(String jsonPath) {}
+
 }
